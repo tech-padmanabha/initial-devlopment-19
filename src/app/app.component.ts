1bc7079fb31f36cccc4126bfb83aa2369df28a18
padmanabha <tech.padmanabha@gmail.com>
2025-08-17 11:50:40 +0530

    added the home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
-  title = 'initial-devlopment-19';
-}
+export class AppComponent {}
